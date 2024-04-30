e2c8812cadece88f64c5761929284308df8d4a17
fuxm00 <fuxm00@vse.cz>
2024-04-30 12:05:38 +0200

    express + hello world

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+import express from 'express'
+
+const port = 4000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
