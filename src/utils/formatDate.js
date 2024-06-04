fc2e8d51f0f697a303cdede1627c67d33435fc04
fuxm00 <fuxm00@vse.cz>
2024-06-04 11:55:08 +0200

    refactoring

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,6 +1,5 @@
 import moment from "moment";
 
 export const formatDate = async function (date, format) {
-    const formatedDate = moment(date).format(format);
-    return formatedDate;
+    return moment(date).format(format);
 }
