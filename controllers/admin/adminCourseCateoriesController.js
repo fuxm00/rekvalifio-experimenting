2dc7d645e5e7bc6fd2a41e8bec23659abe1cd323
fuxm00 <fuxm00@vse.cz>
2024-06-16 12:28:16 +0200

    session u formů

--- controllers/admin/adminCourseCateoriesController.js
+++ controllers/admin/adminCourseCateoriesController.js
@@ -9,36 +9,45 @@ export const adminCoursesCategoriesView = async (req, res) => {
 
     const categories = await getCategoriesWithType()
     const types = await getAllCourseTypes()
+    const sessionCategory = req.session.sessionCategory
 
     res.render("admin/coursesCategories", {
-        title: 'kurzy', categories, marked: "categories", category: null, types
+        title: 'kurzy', categories, marked: "categories", category: null, types, sessionCategory,
     });
 }
 
 export const adminCoursesCategoryView = async (req, res) => {
 
+    req.session.sessionCategory = null
+
     const categoryId = req.params.id;
     const category = await getCategoryById(categoryId);
-    const type = await getTypeById(category.id)
+    const type = category ? await getTypeById(category.typeId) : null
     const categories = await getCategoriesWithType()
     const types = await getAllCourseTypes()
 
     res.render("admin/coursesCategories", {
-        title: 'kurzy', categories, marked: "categories", category: category, types, type
+        title: 'kurzy', categories, marked: "categories", category: category, types, type, sessionCategory: null
     });
 }
 
 export const addCategory = async (req, res) => {
 
     const {categoryTitle: title, categoryContent: content, typeId} = req.body
-    const toastMessages = []
 
-    !title ? toastMessages.push({type: toastTypes.warning, title: "Zadejte název kategorie"}) : null
-    !typeId ? toastMessages.push({type: toastTypes.warning, title: "Zadejte typ kategorie"}) : null
+    const toastMessages = []
+    const sessionCategory = {}
+
+    !title ? toastMessages.push({type: toastTypes.warning, title: "Zadejte název kategorie"}) : sessionCategory.title = title
+    !typeId ? toastMessages.push({type: toastTypes.warning, title: "Zadejte typ kategorie"}) : sessionCategory.typeId = Number(typeId)
+    sessionCategory.content = content
 
     if (toastMessages.length < 1) {
         await createCategory({title, content, typeId})
         toastMessages.push({type: toastTypes.normal, title: "Kategorie přidána"})
+        req.session.sessionCategory = null
+    } else {
+        req.session.sessionCategory = sessionCategory
     }
 
     req.session.toastMessages = toastMessages
@@ -47,6 +56,8 @@ export const addCategory = async (req, res) => {
 }
 
 export const editCategory = async (req, res) => {
+
+    req.session.sessionCategory = null
 
     const {categoryTitle: title, categoryContent: content, typeId} = req.body
     const {id: categoryId} = req.params;
--- controllers/admin/adminCoursesController.js
+++ controllers/admin/adminCoursesController.js
@@ -11,13 +11,16 @@ export const adminCoursesView = async (req, res) => {
 
     const courses = await getCoursesWithCategory()
     const categories = await getAllCourseCategories()
+    const sessionCourse = req.session.sessionCourse
 
     res.render("admin/courses", {
-        title: 'kurzy', courses, course: null, marked: "courses", categories
+        title: 'kurzy', courses, course: null, marked: "courses", categories,sessionCourse
     });
 }
 
 export const adminCourseView = async (req, res) => {
+
+    req.session.sessionCourse = null
 
     const courseId = req.params.id;
     const course = await getCourseById(courseId);
@@ -25,7 +28,7 @@ export const adminCourseView = async (req, res) => {
     const categories = await getAllCourseCategories()
 
     res.render("admin/courses", {
-        title: 'Kurz', course, marked: 'courses', categories, courses
+        title: 'Kurz', course, marked: 'courses', categories, courses, sessionCourse: null
     });
 }
 
@@ -34,14 +37,19 @@ export const addCourse = async (req, res) => {
     const {title, courseContent: content, category: categoryId, price} = req.body
 
     const toastMessages = []
+    const sessionCourse = {}
 
-    !title ? toastMessages.push({type: toastTypes.warning, title: "Zadejte název kurzu"}) : null
-    !categoryId ? toastMessages.push({type: toastTypes.warning, title: "Zadejte kategorii kurzu"}) : null
-    !price ? toastMessages.push({type: toastTypes.warning, title: "Zadejte cenu kurzu"}) : null
+    !title ? toastMessages.push({type: toastTypes.warning, title: "Zadejte název kurzu"}) : sessionCourse.title = title
+    !categoryId ? toastMessages.push({type: toastTypes.warning, title: "Zadejte kategorii kurzu"}) : sessionCourse.categoryId = Number(categoryId)
+    !price ? toastMessages.push({type: toastTypes.warning, title: "Zadejte cenu kurzu"}) : sessionCourse.price = price
+    sessionCourse.content = content
 
     if (toastMessages.length < 1) {
         await createCourse({title, content, categoryId, price})
         toastMessages.push({type: toastTypes.normal, title: "Kurz přidán"})
+        req.session.sessionCourse = null
+    } else {
+        req.session.sessionCourse = sessionCourse
     }
 
     req.session.toastMessages = toastMessages
@@ -50,6 +58,8 @@ export const addCourse = async (req, res) => {
 }
 
 export const editCourse = async (req, res) => {
+
+    req.session.sessionCourse = null
 
     const {title, courseContent: content, category: categoryId, price} = req.body
     const {id: courseId} = req.params;
